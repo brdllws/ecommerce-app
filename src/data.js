6d015ef21abbf95214312dcaef2d1b84e5a2b4fb
Bradley Lewis <brdllws@gmail.com>
2022-01-04 23:37:54 -0500

    Added image gallery for deals

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1593640408182-31c70c8268f5?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=alienware-Hpaq-kBcYHk-unsplash.jpg&w=1920",
+      title: "CYBER MONDAY SALE",
+      desc: "40% OFF ALL ALIENWARE PRODUCTS",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1461151304267-38535e780c79?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=jens-kreuter-ngMtsE5r9eI-unsplash.jpg&w=1920",
+      title: "TV SALE",
+      desc: "DON'T COMPROMISE! GET FLAT 30% OFF ON NEW SONY TVs.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=jeshoots-com-eCktzGjC-iU-unsplash.jpg&w=1920",
+      title: "BUY 1 GET 1 FREE!",
+      desc: "FOR A LIMITED TIME, BUY ANY GAMING CONTROLLER AND GET ANOTHER FOR FREE",
+      bg: "fbf0f4",
+    },
+  ];
