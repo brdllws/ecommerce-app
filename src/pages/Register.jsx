b385ce3099e6e23ac0f6ddbd1be9f43be1a8bc3a
Bradley Lewis <brdllws@gmail.com>
2022-01-14 13:16:24 -0500

    Added individual product page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,71 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.5),
+        rgba(255, 255, 255, 0.5)),
+        url("https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=marvin-meyer-SYTO3xs06fU-unsplash.jpg&w=1920")
+        center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Wrapper = styled.div`
+    padding: 20px;
+    width: 40%;
+    background-color: white;
+`;
+
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0 0;
+    padding: 10px;
+`;
+
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    margin-top: 15px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`;
+
+const Register = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>Create Account</Title>
+                <Form>
+                    <Input placeholder="First Name"/>
+                    <Input placeholder="Last Name"/>
+                    <Input placeholder="Username"/>
+                    <Input placeholder="E-mail"/>
+                    <Input placeholder="Password"/>
+                    <Input placeholder="Confirm Password"/>
+                    <Button>Create Account</Button>
+                </Form>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Register
